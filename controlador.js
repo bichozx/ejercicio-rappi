5018a5147408ff724a3807f899792f0ac0fad929
bichozx <61300748+bichozx@users.noreply.github.com>
2020-06-29 00:28:36 -0500

    se realiza evento click
    se realiza el ejercicio rappi y se crea el evento click

---
+++ controlador.js
@@ -0,0 +1,21 @@
+let opciones= document.getElementsByName('tortaopostre')[0];
+
+
+let exito= function(e){
+
+    if(opciones.value=="POSTRE"){
+        alert ("tiene 20% de descuento");
+
+    }else{}
+        
+
+
+    };
+
+
+
+let validar= document.getElementById('pedirdomicilio');
+validar.addEventListener("click", exito );
+
+
+   
